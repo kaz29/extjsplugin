46bdbc5b39cd67e8abe520adc4ceb6d73a39f21a
kaz29 <kaz@e-2.co.jp>
2010-03-13 13:18:56 +0900

    Add ExtJS Engine Helper.

---
+++ tests/cases/helpers/extjs_engine.test.php
@@ -0,0 +1,205 @@
+<?php
+App::import('Helper', array('Html', 'Js', 'extjs.ExtjsEngine'));
+
+class ExtjsEngineHelperTestCase extends CakeTestCase {
+/**
+ * startTest
+ *
+ * @return void
+ */
+	function startTest() {
+		$this->Extjs =& new ExtjsEngineHelper();
+	}
+
+/**
+ * end test
+ *
+ * @return void
+ */
+	function endTest() {
+		unset($this->Extjs);
+	}
+	
+/**
+ * test selector method
+ *
+ * @return void
+ */
+	function testSelector() {
+		$result = $this->Extjs->get('#content');
+		$this->assertEqual($result, $this->Extjs);
+		$this->assertEqual($this->Extjs->selection, 'Ext.get("content")');
+
+		$result = $this->Extjs->get('document');
+		$this->assertEqual($result, $this->Extjs);
+		$this->assertEqual($this->Extjs->selection, 'Ext.get(document)');
+
+		$result = $this->Extjs->get('window');
+		$this->assertEqual($result, $this->Extjs);
+		$this->assertEqual($this->Extjs->selection, 'Ext.select(window)');
+
+		$result = $this->Extjs->get('ul');
+		$this->assertEqual($result, $this->Extjs);
+		$this->assertEqual($this->Extjs->selection, 'Ext.select("ul")');
+	}
+	
+/**
+ * test event binding
+ *
+ * @return void
+ */
+	function testEvent() {
+		$this->Extjs->get('#myLink');
+		$result = $this->Extjs->event('click', 'doClick', array('wrap' => false));
+		$expected = 'Ext.get("myLink").on("click", doClick);';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->event('click', 'this.show();', array('stop' => false));
+		$expected = 'Ext.get("myLink").on("click", function () {this.show();});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->event('click', 'this.hide();');
+		$expected = 'Ext.get("myLink").on("click", function () {this.hide();'."\n".'return false;});';
+		$this->assertEqual($result, $expected);
+	}
+	
+/**
+ * test dom ready event creation
+ *
+ * @return void
+ */
+	function testDomReady() {
+		$result = $this->Extjs->domReady('foo.name = "bar";');
+		$expected = 'Ext.onReady(function () {foo.name = "bar";});';
+		$this->assertEqual($result, $expected);
+	}
+
+/**
+ * test Each method
+ *
+ * @return void
+ */
+	function testEach() {
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->each('el.hide();');
+		$expected = 'Ext.get("foo").each(function (el) {el.hide();});';
+		$this->assertEqual($result, $expected);
+		
+		$this->Extjs->get('foo');
+		$result = $this->Extjs->each('el.hide();');
+		$expected = 'Ext.select("foo").each(function (el) {el.hide();});';
+		$this->assertEqual($result, $expected);
+	}
+/**
+ * test Effect generation
+ *
+ * @return void
+ */
+	function testEffect() {
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('slideIn');
+		$expected = 'Ext.get("foo").slideIn(\'r\', {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+		
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('slideOut');
+		$expected = 'Ext.get("foo").slideOut(\'r\', {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('slideDown');
+		$expected = 'Ext.get("foo").slideOut(\'b\', {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('slideUp');
+		$expected = 'Ext.get("foo").slideOut(\'t\', {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('puff');
+		$expected = 'Ext.get("foo").puff({duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('switchOff');
+		$expected = 'Ext.get("foo").switchOff({duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('fadeIn');
+		$expected = 'Ext.get("foo").fadeIn({duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('fadeOut');
+		$expected = 'Ext.get("foo").fadeOut({duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('frame');
+		$expected = 'Ext.get("foo").frame(\'FF0000\', 3, {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+
+		$this->Extjs->get('#foo');
+		$result = $this->Extjs->effect('highlight');
+		$expected = 'Ext.get("foo").highlight(\'FF0000\', {duration: 0.5});';
+		$this->assertEqual($result, $expected);
+	}
+/**
+ * Test Request Generation
+ *
+ * @return void
+ */
+	function testRequest() {
+		$result = $this->Extjs->request(array('controller' => 'posts', 'action' => 'view', 1));
+		$expected = 'Ext.Ajax.request({url:"\\/posts\\/view\\/1"});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->request(array('controller' => 'posts', 'action' => 'view', 1), array(
+			'update' => '#content',
+			'wrapCallbacks' => true,
+		));
+		$expected = 'Ext.Ajax.request({success:function (data, textStatus) {Ext.get("content").update(data.responseText);}, url:"\/posts\/view\/1"});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->request('/people/edit/1', array(
+			'method' => 'post',
+			'success' => 'doSuccess',
+			'error' => 'handleError',
+			'data' => array('name' => 'jim', 'height' => '185cm'),
+			'wrapCallbacks' => false
+		));
+		$expected = 'Ext.Ajax.request({failure:handleError, method:"post", params:"name=jim&height=185cm", success:doSuccess, url:"\\/people\\/edit\\/1"});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->request('/people/edit/1', array(
+			'update' => '#updated',
+			'success' => 'doFoo',
+			'method' => 'post',
+			'wrapCallbacks' => false
+		));
+		$expected = 'Ext.Ajax.request({method:"post", success:function (data, textStatus) {Ext.get("updated").update(data.responseText);}, url:"\\/people\\/edit\\/1"});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->request('/people/edit/1', array(
+			'update' => '#updated',
+			'success' => 'doFoo',
+			'method' => 'post',
+			'dataExpression' => true,
+			'data' => 'Ext.Ajax.serializeForm(Ext.get("someId"))',
+			'wrapCallbacks' => false
+		));
+		$expected = 'Ext.Ajax.request({method:"post", params:Ext.Ajax.serializeForm(Ext.get("someId")), success:function (data, textStatus) {Ext.get("updated").update(data.responseText);}, url:"\\/people\\/edit\\/1"});';
+		$this->assertEqual($result, $expected);
+
+		$result = $this->Extjs->request('/people/edit/1', array(
+			'success' => 'doFoo',
+			'method' => 'post',
+			'dataExpression' => true,
+			'data' => 'Ext.Ajax.serializeForm(Ext.get("someId"))',
+		));
+		$expected = 'Ext.Ajax.request({method:"post", params:Ext.Ajax.serializeForm(Ext.get("someId")), success:function (data, textStatus) {doFoo}, url:"\\/people\\/edit\\/1"});';
+		$this->assertEqual($result, $expected);
+	}
+}
---
+++ views/helpers/extjs_engine.php
@@ -0,0 +1,215 @@
+<?php
+/**
+ * ExtJS Engine Helper for JsHelper
+ *
+ * Provides ExtJS specific Javascript for JsHelper.
+ *
+ * PHP versions 4 and 5
+ *
+ * @copyright       Copyright 2010, kaz29.
+ * @link            http://d.hatena.ne.jp/kaz_29/
+ * @package         extjsplugin
+ * @subpackage      extjsplugin.view.helpers
+ * @license         MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Helper', 'Js');
+
+class ExtjsEngineHelper extends JsBaseEngineHelper
+{
+/**
+ * Option mappings for ExtJS
+ *
+ * @var array
+ */
+	var $_optionMap = array(
+		'request' => array(
+			'error' => 'failure',
+			'data' => 'params',
+		),
+	);
+
+/**
+ * callback arguments lists
+ *
+ * @var string
+ */
+	var $_callbackArguments = array(
+		'request' => array(
+			'beforeSend' => 'XMLHttpRequest',
+			'error' => 'XMLHttpRequest, textStatus, errorThrown',
+			'success' => 'data, textStatus',
+			'complete' => 'XMLHttpRequest, textStatus',
+			'xhr' => ''
+		)
+	);
+	
+/**
+ * Add an event to the script cache. Operates on the currently selected elements.
+ *
+ * ### Options
+ *
+ * - 'wrap' - Whether you want the callback wrapped in an anonymous function. (defaults true)
+ * - 'stop' - Whether you want the event to stopped. (defaults true)
+ *
+ * @param string $type Type of event to bind to the current dom id
+ * @param string $callback The Javascript function you wish to trigger or the function literal
+ * @param array $options Options for the event.
+ * @return string completed event handler
+ */
+	function event($type, $callback, $options = array())
+	{
+		$defaults = array('wrap' => true, 'stop' => true);
+		$options = array_merge($defaults, $options);
+
+		$function = 'function () {%s}';  	
+		if ($options['wrap'] && $options['stop']) {
+			$callback .= "\nreturn false;";
+		}
+		if ($options['wrap']) {
+			$callback = sprintf($function, $callback);
+		}
+		
+		if ( $type === 'onReady' ) {
+		  return sprintf('Ext.%s(%s);', $type, $callback);
+    } else {
+		  return sprintf('%s.on("%s", %s);', $this->selection, $type, $callback) ;
+    }
+	}
+
+/**
+ * Create a domReady event. This is a special event in many libraries
+ *
+ * @param string $functionBody The code to run on domReady
+ * @return string completed domReady method
+ */
+	function domReady($functionBody)
+	{
+		return $this->event('onReady', $functionBody, array('stop' => false));
+	}
+	
+/**
+ * Create javascript selector for a CSS rule
+ *
+ * @param string $selector The selector that is targeted
+ * @return object instance of $this. Allows chained methods.
+ */
+	function get($selector)
+	{
+		if ($selector == 'window' ) {
+	    $this->selection = 'Ext.select('.$selector.')' ;
+		} else if ( $selector == 'document') {
+	    $this->selection = 'Ext.get('.$selector.')' ;
+	  } else if ( $selector[0] === '#' ) {
+	    $this->selection = 'Ext.get("'.substr($selector,1).'")' ;
+	  } else {
+	    $this->selection = 'Ext.select("'.$selector.'")' ;
+	  }
+	  
+	  return $this ;
+	}	
+
+/**
+ * Create an iteration over the current selection result.
+ *
+ * @param string $method The method you want to apply to the selection
+ * @param string $callback The function body you wish to apply during the iteration.
+ * @return string completed iteration
+ */
+	function each($callback) 
+	{
+		return $this->selection.'.each(function (el) {' . $callback . '});';
+	}
+	
+/**
+ * Trigger an Effect.
+ *
+ * @param string $name The name of the effect to trigger.
+ * @param array $options Array of options for the effect.
+ * @return string completed string with effect.
+ * @see JsBaseEngineHelper::effect()
+ */
+	function effect($name, $options = array()) {
+		$speed = .5;
+		if (isset($options['speed'])) {
+			$speed = $options['speed'];
+		}
+		$anchor = 'r';
+		if (isset($options['anchor']) && in_array($options['anchor'], array('r', 'l', 't', 'b'))) {
+			$anchor = $options['anchor'];
+		}
+		$color = 'FF0000';
+		if (isset($options['color'])) {
+			$color = $options['color'];
+		}
+		$count = 3;
+		if (isset($options['count'])) {
+			$count = $options['count'];
+		}
+		
+		$effect = '';
+		switch ($name) {
+			case 'slideIn':
+				$effect = ".$name('$anchor', {duration: {$speed}});";
+			break ;
+			case 'slideOut':
+				$effect = ".$name('$anchor', {duration: {$speed}});";
+			break ;
+			case 'slideDown':
+				$effect = ".slideOut('b', {duration: {$speed}});";
+			break ;
+			case 'slideUp':
+				$effect = ".slideOut('t', {duration: {$speed}});";
+			break ;
+			case 'puff':
+			case 'switchOff':
+ 			case 'fadeIn':
+			case 'fadeOut':
+				$effect = ".$name({duration: {$speed}});";
+			break ;
+			case 'frame':
+				$effect = ".$name('$color', $count, {duration: {$speed}});";
+			break ;
+			case 'highlight':
+				$effect = ".$name('$color', {duration: {$speed}});";
+			break ;
+		}
+		return $this->selection.$effect;
+	}
+	
+/**
+ * Create an $.ajax() call.
+ *
+ * If the 'update' key is set, success callback will be overridden.
+ *
+ * @param mixed $url
+ * @param array $options
+ * @return string The completed ajax call.
+ */
+	function request($url, $options = array()) {
+		$url = $this->url($url);
+		$options = $this->_mapOptions('request', $options);
+		if (isset($options['params']) && is_array($options['params'])) {
+			$options['params'] = $this->_toQuerystring($options['params']);
+		}
+		$options['url'] = $url;
+		if (isset($options['update'])) {
+		  $update = ($options['update'][0] === '#')? substr($options['update'],1): $options['update'] ;
+			$wrapCallbacks = isset($options['wrapCallbacks']) ? $options['wrapCallbacks'] : true;
+			if ($wrapCallbacks) {
+				$success = 'Ext.get("' . $update . '").update(data.responseText);';
+			} else {
+				$success = 'function (data, textStatus) {Ext.get("' . $update . '").update(data.responseText);}';
+			}
+			$options['success'] = $success;
+			unset($options['update']);
+		}
+		$callbacks = array('success', 'failure', 'callback');
+		if (isset($options['dataExpression'])) {
+			$callbacks[] = 'params';
+			unset($options['dataExpression']);
+		}
+		$options = $this->_prepareCallbacks('request', $options);
+		$options = $this->_parseOptions($options, $callbacks);
+		return 'Ext.Ajax.request({' . $options .'});';
+  }
+}
